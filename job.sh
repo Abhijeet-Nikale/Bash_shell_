44515556bc7c8e345f82061a1059a2391e056b8a
Abhijeet-Nikale <32222771+Abhijeet-Nikale@users.noreply.github.com>
2018-12-08 21:00:51 -0500

    all the job portals

---
+++ job.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+firefox https://www.dice.com/dashboard/profiles/active https://www.monster.com/account/homepage/ https://www.indeed.com/ https://www.ziprecruiter.com/candidate/suggested-jobs https://neuvoo.com/# https://www.glassdoor.com/index.htm https://www.careerbuilder.com/ https://www.engineerjobs.com/ 
