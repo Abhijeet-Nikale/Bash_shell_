838f09b0874691f5c73c747cd24f4718023d4f1f
Abhijeet-Nikale <32222771+Abhijeet-Nikale@users.noreply.github.com>
2019-04-12 18:12:11 -0700

    Update job.sh

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-firefox https://www.dice.com/dashboard/profiles/active https://www.monster.com/account/homepage/ https://www.indeed.com/ https://www.ziprecruiter.com/candidate/suggested-jobs https://neuvoo.com/# https://www.glassdoor.com/index.htm https://www.careerbuilder.com/ https://www.engineerjobs.com/ 
+firefox https://www.dice.com/dashboard/profiles/active https://www.monster.com/account/homepage/ https://www.indeed.com/ https://www.ziprecruiter.com/candidate/suggested-jobs https://neuvoo.com/# https://www.glassdoor.com/index.htm https://www.careerbuilder.com/ https://www.engineerjobs.com/ https://www.randstadusa.com/jobs/search/q-network/ 
